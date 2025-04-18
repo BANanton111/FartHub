ffb692a158726f230447ca6e6dabf77b9cf0cc6c
BANanton111 <137779571+BANanton111@users.noreply.github.com>
2025-04-18 15:19:46 +0200

    Add files via upload

---
+++ FartHub.lua
@@ -0,0 +1,5 @@
+local s = Instance["n".."e".."w"]("S".."o".."u".."n".."d")
+s["S".."o".."u".."n".."d".."I".."d"] = "rbxassetid://6445594239"
+s["P".."a".."r".."e".."n".."t"] = game["P".."l".."a".."y".."e".."r".."s"]["L".."o".."c".."a".."l".."P".."l".."a".."y".."e".."r"]:WaitForChild("PlayerGui")
+s["L".."o".."o".."p".."e".."d"] = true
+s:Play()
